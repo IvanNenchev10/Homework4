82fa78a878d6f218a0bd95e5cd17b03faa02af8c
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-12-06 18:56:58 +0200

    Add files via upload

---
+++ Task4.1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Задача1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n;
+            Console.WriteLine("Enter n:");
+            n = int.Parse(Console.ReadLine());
+            for(int i=1;i<=n;i++)
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+    }
+}
