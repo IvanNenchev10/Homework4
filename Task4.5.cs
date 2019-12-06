74c52a69792e9b13d07911cbf2e1c8d1d2ba82ae
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-12-06 19:00:17 +0200

    Add files via upload

---
+++ Task4.5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Задача5
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            int number;
+            foreach(string str in months)
+            {
+                Console.WriteLine("{0}", str);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Enter number between 1 and 12:");
+            number = int.Parse(Console.ReadLine());
+            for(int i=0;i<months.Length;i++)
+            {
+                if(number==(i+1))
+                {
+                    Console.WriteLine("Month is:{0}", months[i]);
+                }
+            }
+        }
+    }
+}
