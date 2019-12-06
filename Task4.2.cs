a54474d7dcd4e740083b821b1616c68b281dc74d
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-12-06 18:57:54 +0200

    Add files via upload

---
+++ Task4.2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Задача2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n;
+            Console.WriteLine("Enter number of elements:");
+            n = int.Parse(Console.ReadLine());
+            for(int i=1;i<=n;i++)
+            {
+                if(!(i%3==0 && i%7==0))
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+        }
+    }
+}
