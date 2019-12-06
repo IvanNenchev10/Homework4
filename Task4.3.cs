e99f32c1945c0d1a6c5dd403a9a0530e7144ee54
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-12-06 18:58:48 +0200

    Add files via upload

---
+++ Task4.3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Задача3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b, c;  //Purvi nachin
+            Console.WriteLine("Enter a:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter c:");
+            c = int.Parse(Console.ReadLine());
+            if(a>b && a>c)
+            {
+                Console.WriteLine("Biggest number is:{0}", a);
+            }
+            else if(b>a && b>c)
+            {
+                Console.WriteLine("Biggest number is:{0}", b);
+            }
+            else if(c>a && c>b)
+            {
+                Console.WriteLine("Biggest number is:{0}", c);
+            }
+            else
+            {
+                Console.WriteLine("There is no one biggest number");
+            }
+            /*int[] arr = new int[3]; //Vtori nachin
+            int max;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter element {0}:", i + 1);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            max = arr[0];
+            for (int i = 1; i < 3; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine("Max number is:{0}", max);*/
+
+        }
+    }
+}
